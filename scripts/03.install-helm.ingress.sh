51ef048d9a1df8f73e864a3b55af6bb5daa9c0c6
Raul <rauljmz@gmail.com>
2018-12-04 10:42:15 +0000

    Fix typo in ingress creation

--- scripts/03.install-helm.ingress.sh
+++ scripts/03.install-helm.ingress.sh
@@ -8,4 +8,4 @@ while [ ! helm version &> /dev/null ]; do
     sleep 1s
 done
 
-helm install stable/nginx-ingress --namespace ingress --name ingress  --set rbac.create="true",controler.extraArgs="enable-ssl-passthrough",controller.nodeSelector."beta\.kubernetes\.io/os"="linux",defaultBackend.nodeSelector."beta\.kubernetes\.io/os"="linux"
+helm install stable/nginx-ingress --namespace ingress --name ingress  --set rbac.create="true",controller.extraArgs="enable-ssl-passthrough",controller.nodeSelector."beta\.kubernetes\.io/os"="linux",defaultBackend.nodeSelector."beta\.kubernetes\.io/os"="linux"
