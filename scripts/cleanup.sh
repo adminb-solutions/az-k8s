8b838c4418c6e1089ec6d8d0a933fbc36454b24e
Raul Jimenez <rauljmz@gmail.com>
2019-02-26 19:29:12 +0000

    Login on cleanup if account not available

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+az account show &> /dev/null || az login
+
 echo "Removing service account"
 az ad sp delete --id http://${RESOURCE_GROUP}_k8s
 echo "Deleting all assets in resource group ${RESOURCE_GROUP}"
