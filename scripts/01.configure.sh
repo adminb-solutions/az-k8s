b826fe45871d3cd5b532e22ceaad700039712a25
Raul Jimenez <rauljmz@gmail.com>
2019-02-26 16:07:25 +0000

    Remove surplus spaces

--- scripts/01.configure.sh
+++ scripts/01.configure.sh
@@ -25,7 +25,7 @@ fi
 
 # Create a service account for the cluster - only has access to the resource group
 # The cluster needs this account to be able to create public IPs for Load balancers, or storage for volumes
-if [ ! -e /root/.azure/k8s.${RESOURCE_GROUP}.account.json ]; then    
+if [ ! -e /root/.azure/k8s.${RESOURCE_GROUP}.account.json ]; then
     SUBSCRIPTION_ID=`az account show --query 'id' -o tsv`
     ACCOUNT_ID=http://${RESOURCE_GROUP}_k8s
     az ad sp create-for-rbac --name $ACCOUNT_ID --scopes "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}" > /root/.azure/k8s.${RESOURCE_GROUP}.account.json
@@ -33,7 +33,7 @@ fi
 
 # Create a key for the linux machines. You can then login to the master node doing ssh
 if [ ! -e /root/.azure/id_rsa ]; then
-    ssh-keygen -t rsa -b 4096 -N "" -f /root/.azure/id_rsa    
+    ssh-keygen -t rsa -b 4096 -N "" -f /root/.azure/id_rsa
 fi
 mkdir -p /root/.ssh
 cp /root/.azure/id_rsa* /root/.ssh
